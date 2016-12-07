eee30224839ec7e19a8b7a7cdc726ffae48c8907
sajjad0098 <pashangkia@yahoo.com>
2016-12-07 01:00:39 -0800

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -231,21 +231,25 @@ function create_config( )
     "supergroup",
     "language",
     "whitelist",
-    "tools"
+    "tools",
+    "chat",
+    "semat",
+    "mute-all",
+    "time",
+    "dler"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
 This is an original bot and based on (AdvanSource, version 4 final).
 Copyright all right reserved and you must respect all laws.
 
-Source: https://github.com/janlou/AdvanSource
-Channel: @AdvanTm
-Messenger: @Advanbot
-Creator: @janlou
-Site: http://StoreVps.ir
+Source: https://github.com/sajjad0098/hitler_tg
+Channel: @robot_list
+Messenger: @sajjad_jitler
+Creator: @sajjad_jitler
 ]],
     --Start rate:
     Group_rate = [[]],
